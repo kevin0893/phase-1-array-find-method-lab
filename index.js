2e63b85df464e04df076377f1abd74af576888d6
+ <cheruiyotke95@gmail.com>
2024-10-08 11:51:35 +0300

    array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+// code your solution here
+const records = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+//
+function superbowlWin(record) {
+  const winningRecord = record.find(entry => entry.result === "W");
+  return winningRecord ? winningRecord.year : undefined;
+}
